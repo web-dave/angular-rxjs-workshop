c0319c07bc5083c2a6be2f53e2efde9a1f5daabc
web-dave <dave@webdave.de>
2022-01-31 11:04:45 +0100

    click

--- apps/client/cypress/integration/todos/todos.spec.ts
+++ apps/client/cypress/integration/todos/todos.spec.ts
@@ -1,10 +1,23 @@
 describe('Todos', () => {
-  it('should be visible', () => {
+  before(() => {
     cy.visit('/');
+  });
+  it('should be visible', () => {
     cy.contains('Todos');
-    // cy.contains('sandbox app is running!');
+
     cy.wait(3000);
     cy.get('dos-todo-checker').contains('Buy');
     cy.get('dos-todo-checker').should('have.length', 7);
+  });
+
+  it('should check uncheck one item', () => {
+    cy.wait(3000);
+    cy.get('dos-todo-checker')
+      .contains('Buy')
+      .should('have.class', 'todo--is-done');
+    cy.get('dos-todo-checker').contains('Buy').click();
+    cy.get('dos-todo-checker')
+      .contains('Buy')
+      .should('not.have.class', 'todo--is-done');
   });
 });
