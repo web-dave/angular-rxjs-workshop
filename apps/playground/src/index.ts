b505ac17a32fb67666d0436a67d15002db17fd7a
web-dave <dave@webdave.de>
2022-02-07 14:27:25 +0100

    playground

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,37 @@
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
+
+// Create observable
+const helloWorld$ = timer(5000, 2000);
+// const helloWorld$ = new Observable(function subscribe(observer) {
+//   //   observer.next(1);
+//   //   observer.next(2);
+//   //   observer.next(3);
+//   //   observer.error('Aaaaargh');
+//   //   observer.complete();
+//   //   observer.next(4);
+//   let i = 0;
+//   const interv = setInterval(() => {
+//     i++;
+//     console.log('inter', i);
+//     observer.next(i);
+//   }, 1000);
+
+//   return function unsubscribe() {
+//     clearInterval(interv);
+//   };
+// });
+
+// Subscribe to an observable
+const sub = helloWorld$.subscribe({
+  next(x) {
+    console.log(x);
+  },
+  error(err) {
+    console.error(err);
+  },
+  complete() {
+    console.log('done');
+  }
+});
+
+setTimeout(() => sub.unsubscribe(), 8000);
