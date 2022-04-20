76d39ab5b32217b90ab4246d794472a911611081
web-dave <dave@webdave.de>
2022-04-20 11:23:43 +0200

    interval (delay output)

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,13 +1,23 @@
 import { Observable } from 'rxjs';
 
 const obs$ = new Observable(function subscribe(observer) {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.next(4);
-  observer.complete();
-  observer.error(new Error('Meeeeh'));
-  observer.next(4.1);
+  let i = 0;
+  const intv = setInterval(() => {
+    i++;
+    observer.next(i);
+    console.log('inner', i);
+  }, 1000);
+
+  setTimeout(() => {
+    observer.complete();
+    clearInterval(intv);
+  }, 3000);
+  //   observer.next(2);
+  //   observer.next(3);
+  //   observer.next(4);
+  //   observer.complete();
+  //   observer.error(new Error('Meeeeh'));
+  //   observer.next(4.1);
 });
 
 obs$.subscribe({
@@ -15,3 +25,13 @@ obs$.subscribe({
   error: (err) => console.error(err),
   complete: () => console.info('Fertsch')
 });
+
+const obj = {
+  antwort: 42,
+  antworten: function () {
+    return this.antwort;
+  },
+  antworten2: () => {
+    return this?.antwort;
+  }
+};
