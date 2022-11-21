47b8a45b98538f40fdbcd9c3c4b88f07d6930482
web-dave <dave@webdave.de>
2022-11-21 11:48:25 +0100

    playground

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,46 @@
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
+// console.log(1, 2, 3, 4);
+
+// const helloWorld$ = new Observable(function subscribe(observer) {
+//   let i = 0;
+//   const int = setInterval(() => {
+//     console.info('producer', i);
+//     observer.next(i);
+//     i++;
+//   }, 1000);
+//   //   observer.next(1);
+//   //   observer.next(2);
+//   //   observer.next(3);
+//   //   observer.next(4);
+//   //   observer.complete();
+//   return function unsubscribe() {
+//     console.log('unsub');
+//     clearInterval(int);
+//   };
+// });
+
+const helloWorld$ = timer(5000, 2000);
+// helloWorld$.
+const sub = helloWorld$.subscribe({ next: (data) => console.log(data) });
+setTimeout(() => sub.unsubscribe(), 10000);
+
+const myObservable = {
+  oberservers: [],
+  subscribe: function (observer) {
+    this.oberservers.push(observer);
+    ////////////////////////////////////////
+    // producer
+    fetch('')
+      .then((data) => {
+        this.oberservers.forEach((element) => {
+          element.next(data);
+        });
+      })
+      .catch((err) =>
+        this.oberservers.forEach((element) => {
+          element.error(err);
+        })
+      );
+    /////////////////////////////////////////////
+  }
+};
