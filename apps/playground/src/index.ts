e07e4113cea1527bf71c58177a8600d3521c0a7a
web-dave <dave@webdave.de>
2022-12-01 11:27:40 +0100

    interval, unsubscribe

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -15,15 +15,27 @@ const myObservable = {
 // myObservable.subscribe({ next: (i) => console.log(i) });
 
 const O$ = new Observable(function subscribe(observer) {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.next(4);
-  observer.error(new Error('Meeeeh!'));
-  observer.next(5);
+  // observer.next(1);
+  // observer.next(2);
+  // observer.next(3);
+  // observer.next(4);
+  // observer.error(new Error('Meeeeh!'));
+  // observer.next(5);
+  const interval = setInterval(() => {
+    console.log('Interval!');
+
+    observer.next('Moin');
+  }, 1000);
+  return function unsunbscribe() {
+    console.log('Unsubscribe');
+
+    clearInterval(interval);
+  };
 });
-O$.subscribe({
+const o$ = O$.subscribe({
   next: (data) => console.log('NEXT:', data),
   error: (data) => console.log('error:', data),
   complete: () => console.log('complet')
 });
+
+setTimeout(() => o$.unsubscribe(), 5000);
