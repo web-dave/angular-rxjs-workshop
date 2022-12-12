310a75020ecc303dd9de92ab213ec0739df97443
web-dave <dave@webdave.de>
2022-12-12 10:12:48 +0100

    task 1

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,19 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+const myObs$ = new Observable((observer) => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.next(4);
+  observer.complete();
+  // observer.error(new Error('Ouch!'));
+  observer.next(5);
+});
+
+const myObserver: Observer<number> = {
+  next: (data) => console.log(data),
+  error: (err) => console.error(err),
+  complete: () => console.info('Done')
+};
+
+myObs$.subscribe(myObserver);
