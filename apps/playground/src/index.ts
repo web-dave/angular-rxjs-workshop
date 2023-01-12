34ae65fa42b6ff2cd25073f9aed5a4f614a38d3e
web-dave <dave@webdave.de>
2023-01-12 11:31:13 +0100

    [Playground] Push Numbers

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,5 +1,21 @@
 import { Observable } from 'rxjs';
 
+const myObserveable = new Observable(function subscribe(observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  // observer.complete();
+  observer.error('Ouch!');
+  observer.next(4);
+});
+
+const subscription = myObserveable.subscribe({
+  next: (n) => console.log(n),
+  error: (err) => console.error(err),
+  complete: () => console.info('Complete!')
+});
+
+/*
 const myObs = {
   observer: null,
   subscribe: (observer) => {
@@ -14,3 +30,4 @@ const sub = myObs.subscribe({
   next: (i) => console.info(i)
 });
 setTimeout(() => sub(), 5000);
+*/
