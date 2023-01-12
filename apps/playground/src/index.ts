e818bff6c715e5895bd5ebc8483555ee23ce535d
web-dave <dave@webdave.de>
2023-01-12 11:01:19 +0100

    Observable explained

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,16 @@
 import { Observable } from 'rxjs';
+
+const myObs = {
+  observer: null,
+  subscribe: (observer) => {
+    myObs.observer = observer;
+    setInterval(() => myObs.observer?.next('Hallo!'), 1500);
+    // setTimeout(() => myObs.observer.next('Hallo!'), 1500);
+    // setTimeout(() => myObs.observer.next('Adesso'), 1900);
+    return () => (myObs.observer = null);
+  }
+};
+const sub = myObs.subscribe({
+  next: (i) => console.info(i)
+});
+setTimeout(() => sub(), 5000);
