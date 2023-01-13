77b3d17c46bb676f03636b79af70742b3a9d1cfc
web-dave <dave@webdave.de>
2023-01-13 12:28:11 +0100

    distinctUntilChange

--- apps/client/src/app/todos/todo-settings.service.ts
+++ apps/client/src/app/todos/todo-settings.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { scan, shareReplay } from 'rxjs/operators';
+import { distinctUntilChanged, scan, shareReplay, tap } from 'rxjs/operators';
 
 export interface TodoSettingsOptions {
   isPollingEnabled: boolean;
@@ -16,6 +16,13 @@ export class TodoSettings {
 
   settings$ = this.settings$$.pipe(
     scan((prev, next) => ({ ...prev, ...next })),
+    tap((data) => console.table(data)),
+    distinctUntilChanged((previous, current) => {
+      return (
+        previous.isPollingEnabled === current.isPollingEnabled &&
+        previous.pollingInterval === current.pollingInterval
+      );
+    }),
     shareReplay(1)
   );
 
