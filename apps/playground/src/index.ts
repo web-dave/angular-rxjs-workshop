9779cfe6e9272c8638049f77b5eb83aae373acd3
web-dave <dave@webdave.de>
2023-04-24 10:57:38 +0200

    interval

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -2,11 +2,13 @@ import { Observable } from 'rxjs';
 // console.log('Moin');
 
 const myObservable = new Observable(function subscribe(observer) {
-  observer.next(1);
-  observer.next(2);
-  observer.error('Arrgh');
-  observer.next(3);
-  observer.next(4);
+  setInterval(() => {
+    observer.next(1);
+  }, 1000);
+  // observer.next(2);
+  // observer.error('Arrgh');
+  // observer.next(3);
+  // observer.next(4);
 });
 
 myObservable.subscribe({
