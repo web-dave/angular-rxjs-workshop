c73daebecac0eef8394a7ddb020a82103d6a5222
web-dave <dave@webdave.de>
2023-04-24 10:29:44 +0200

    push numbers

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,16 @@
 import { Observable } from 'rxjs';
+// console.log('Moin');
+
+const myObservable = new Observable(function subscribe(observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.error('Arrgh');
+  observer.next(3);
+  observer.next(4);
+});
+
+myObservable.subscribe({
+  next: (data) => console.log(data),
+  error: (err) => console.error(err),
+  complete: () => console.info('Done')
+});
