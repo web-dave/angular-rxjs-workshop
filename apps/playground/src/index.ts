42d81a46051289367411ad35da34846b5a16120d
web-dave <dave@webdave.de>
2023-04-24 11:46:40 +0200

    timer

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,23 +1,34 @@
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 // console.log('Moin');
 
-const myObservable = new Observable(function subscribe(observer) {
-  const interval = setInterval(() => {
-    observer.next(1);
-  }, 1000);
-  // observer.next(2);
-  // observer.error('Arrgh');
-  // observer.next(3);
-  // observer.next(4);
-  return function unsubscribe() {
-    clearInterval(interval);
-  };
-});
+// const myObservable = new Observable(function subscribe(observer) {
+//   const interval = setInterval(() => {
+//     observer.next(1);
+//   }, 1000);
+//   // observer.next(2);
+//   // observer.error('Arrgh');
+//   // observer.next(3);
+//   // observer.next(4);
+//   return function unsubscribe() {
+//     clearInterval(interval);
+//   };
+// });
+
+const myObservable = timer(3000, 1000);
 
 const foo = myObservable.subscribe({
-  next: (data) => console.log(data),
+  next: (data) => console.log('first', data),
   error: (err) => console.error(err),
   complete: () => console.info('Done')
 });
+setTimeout(() => foo.unsubscribe(), 5000);
 
-setTimeout(() => foo.unsubscribe(), 4000);
+setTimeout(() => {
+  const bar = myObservable.subscribe({
+    next: (data) => console.log('second', data),
+    error: (err) => console.error(err),
+    complete: () => console.info('Done')
+  });
+
+  setTimeout(() => bar.unsubscribe(), 5000);
+}, 3000);
