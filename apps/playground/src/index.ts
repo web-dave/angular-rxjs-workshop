e865d1174cadb75d9d8565dbe44133e98da32023
web-dave <dave@webdave.de>
2023-04-24 11:15:50 +0200

    unsubscribe

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -2,17 +2,22 @@ import { Observable } from 'rxjs';
 // console.log('Moin');
 
 const myObservable = new Observable(function subscribe(observer) {
-  setInterval(() => {
+  const interval = setInterval(() => {
     observer.next(1);
   }, 1000);
   // observer.next(2);
   // observer.error('Arrgh');
   // observer.next(3);
   // observer.next(4);
+  return function unsubscribe() {
+    clearInterval(interval);
+  };
 });
 
-myObservable.subscribe({
+const foo = myObservable.subscribe({
   next: (data) => console.log(data),
   error: (err) => console.error(err),
   complete: () => console.info('Done')
 });
+
+setTimeout(() => foo.unsubscribe(), 4000);
