af3d334e42cda1b94c13420e8b7318e7e2d51bc9
web-dave <dave@webdave.de>
2023-04-25 09:51:14 +0200

    error handling

--- apps/client/src/app/todos/todo.service.ts
+++ apps/client/src/app/todos/todo.service.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { interval, Observable } from 'rxjs';
-import { exhaustMap, map, mergeMap, shareReplay, tap } from 'rxjs/operators';
+import { fromEvent, interval, Observable } from 'rxjs';
+import {
+  exhaustMap,
+  map,
+  mergeMap,
+  retry,
+  shareReplay,
+  tap
+} from 'rxjs/operators';
 import { Toolbelt } from './internals';
 import { Todo, TodoApi } from './models';
 import { TodoSettings } from './todo-settings.service';
@@ -10,17 +17,20 @@ const todosUrl = 'http://localhost:3333/api';
 
 @Injectable()
 export class TodoService {
+  foo = 'bar';
   constructor(
     private http: HttpClient,
     private toolbelt: Toolbelt,
     private settings: TodoSettings
   ) {}
 
+  onLine$ = fromEvent(window, 'online');
+
   loadFrequently(): Observable<Todo[]> {
     // TODO: Introduce error handled, configured, recurring, all-mighty stream
 
     return interval(5000).pipe(
-      exhaustMap(() => this.query().pipe()),
+      exhaustMap(() => this.query().pipe(retry({ count: 2, delay: 2000 }))), //('',200)=>this.onLine$
       tap({ error: () => this.toolbelt.offerHardReload() }),
       tap((data) => console.log(data)),
       shareReplay(1)
@@ -29,9 +39,11 @@ export class TodoService {
 
   // TODO: Fix the return type of this method
   private query(): Observable<Todo[]> {
-    return this.http
-      .get<TodoApi[]>(`${todosUrl}`)
-      .pipe(map((data) => data.map((todo) => this.toolbelt.toTodo(todo))));
+    return this.http.get<TodoApi[]>(`${todosUrl}`).pipe(
+      tap((data) => console.log(data[0])),
+      map((data) => data.map((todo) => this.toolbelt.toTodo(todo))),
+      tap((data) => console.log(data[0]))
+    );
     // TODO: Apply mapping to fix display of tasks
   }
 
