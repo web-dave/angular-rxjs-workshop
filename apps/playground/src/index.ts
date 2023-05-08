13d21b7bffd3cd3755b24634f301cc221964a248
web-dave <dave@webdave.de>
2023-05-08 11:04:49 +0200

    INterval

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -15,16 +15,20 @@ const myObservable = {
 };
 
 const numbers$ = new Observable(function subscribe(observer) {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.error('UFF!');
-  observer.next(4);
-  observer.next(5);
+  setInterval(() => {
+    console.log('Interval');
+    observer.next(1);
+  }, 1000);
+  observer.complete();
+  // observer.next(2);
+  // observer.next(3);
+  // observer.error('UFF!');
+  // observer.next(4);
+  // observer.next(5);
 });
 
 const sub = numbers$.subscribe({
-  next: (data) => console.log(data),
+  next: (data) => console.log('NEXT', data),
   error: (err) => console.error('ERROR', err),
   complete: () => console.info('DONE')
 });
