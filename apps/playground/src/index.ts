803edf59c2f6b0a1238bb32ce7caa18862cf61a9
Dave <dave@webdave.de>
2023-08-23 11:44:55 +0200

    timer

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,41 +1,42 @@
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 
-const obs = {
-  value: 0,
-  observer: (v) => {},
-  subscribe: function (observer) {
-    obs.observer = observer;
-    obs.next(0);
-  },
-  next: function (v) {
-    obs.value = v;
-    obs.observer(obs.value);
-  }
-};
-obs.subscribe((value) => console.log(value));
+// const obs = {
+//   value: 0,
+//   observer: (v) => {},
+//   subscribe: function (observer) {
+//     obs.observer = observer;
+//     obs.next(0);
+//   },
+//   next: function (v) {
+//     obs.value = v;
+//     obs.observer(obs.value);
+//   }
+// };
+// obs.subscribe((value) => console.log(value));
 
-obs.next(1);
+// obs.next(1);
 
 // Create observable
-const helloWorld$ = new Observable(function subscribe(observer) {
-  let i = 0;
-  const interval = setInterval(() => {
-    i++;
-    observer.next(i);
-    console.log('Interval value', i);
-  }, 1000);
+// const helloWorld$ = new Observable(function subscribe(observer) {
+//   let i = 0;
+//   const interval = setInterval(() => {
+//     i++;
+//     observer.next(i);
+//     console.log('Interval value', i);
+//   }, 1000);
 
-  return () => {
-    clearInterval(interval);
-  };
-  // observer.next(1);
-  // observer.next(2);
-  // observer.next(3);
-  // observer.next(4);
-  // observer.complete();
-  // observer.error('Help!');
-});
+//   return () => {
+//     clearInterval(interval);
+//   };
+//   // observer.next(1);
+//   // observer.next(2);
+//   // observer.next(3);
+//   // observer.next(4);
+//   // observer.complete();
+//   // observer.error('Help!');
+// });
 
+const helloWorld$ = timer(2000, 1000);
 // Subscribe to an observable
 const sub = helloWorld$.subscribe({
   next(x) {
@@ -51,4 +52,4 @@ const sub = helloWorld$.subscribe({
 
 setTimeout(() => {
   sub.unsubscribe();
-}, 3000);
+}, 5000);
