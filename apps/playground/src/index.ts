ecc8f3ee65f216b925dc55af1f816dc9f4a15204
Dave <dave@webdave.de>
2024-03-20 10:35:32 +0100

    Uncu

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,16 +1,26 @@
 import { Observable, PartialObserver } from 'rxjs';
 console.log('Hallo');
-const myObs = new Observable(function (observer: PartialObserver<number>) {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.next(4);
-  observer.next(5);
-  observer.complete();
+const myObs = new Observable(function (observer: PartialObserver<string>) {
+  const interval = setInterval(() => {
+    observer.next('Hi');
+    console.log(1);
+  }, 1000);
+  return function () {
+    clearInterval(interval);
+  };
+  // observer.next(1);
+  // observer.next(2);
+  // observer.next(3);
+  // observer.error('Arrrrgh!');
+  // observer.next(4);
+  // observer.next(5);
+  // observer.complete();
 });
 
-myObs.subscribe({
+const sub = myObs.pipe().subscribe({
   next: (data) => console.log(data),
   error: (data) => console.error(data),
   complete: () => console.log('Done')
 });
+
+setTimeout(() => sub.unsubscribe(), 5000);
