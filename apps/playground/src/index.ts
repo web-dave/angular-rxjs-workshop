0a3e8093b70069417478a07b48a1330008af9a71
Dave <dave@webdave.de>
2024-03-20 10:10:18 +0100

    Push numbers

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,16 @@
-import { Observable } from 'rxjs';
+import { Observable, PartialObserver } from 'rxjs';
+console.log('Hallo');
+const myObs = new Observable(function (observer: PartialObserver<number>) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.next(4);
+  observer.next(5);
+  observer.complete();
+});
+
+myObs.subscribe({
+  next: (data) => console.log(data),
+  error: (data) => console.error(data),
+  complete: () => console.log('Done')
+});
