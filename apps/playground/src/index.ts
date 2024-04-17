02bcb037c20494920f7991f2ca4cde48f53ce809
Dave <dave@webdave.de>
2024-04-17 11:11:52 +0200

    eigenes Observable gebaut

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,28 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+const counter$ = new Observable((observer: Observer<number>) => {
+  // console.log('Start');
+  let i = 0;
+  const intId = setInterval(() => {
+    console.log('Interval läuft');
+    observer.next(i);
+    i++;
+  }, 1000);
+  return () => {
+    clearInterval(intId);
+  };
+  // observer.next(1);
+  // observer.next(2);
+  // observer.next(3);
+  // observer.next(4);
+  // observer.complete();
+  // observer.error('Argh');
+});
+const sub = counter$.subscribe({
+  next: (data) => console.log(data),
+  error: (e) => console.log(e),
+  complete: () => console.log('Fertig!')
+});
+setTimeout(() => {
+  sub.unsubscribe();
+}, 4000);
