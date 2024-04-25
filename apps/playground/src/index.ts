c309a4491ac86830147794ed903f48595ec4f768
Dave <dave@webdave.de>
2024-04-25 11:06:25 +0200

    unsubscribe

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -4,14 +4,25 @@ const numbers$ = new Observable(function subscribe(
   observer: Partial<Observer<number>>
 ) {
   let i = 0;
-  setInterval(() => {
+  const int = setInterval(() => {
     observer.next(i);
+    console.log('internal', i);
     i++;
   }, 1000);
+
+  return () => {
+    clearInterval(int);
+  };
 });
 
-numbers$.subscribe({
+const sub = numbers$.subscribe({
   next: (v) => console.log(v),
   error: (err) => console.error(err),
   complete: () => console.info('Done')
 });
+
+setTimeout(() => {
+  sub.unsubscribe();
+
+  console.log(sub);
+}, 5000);
