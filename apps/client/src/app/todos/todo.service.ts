15e1786ed76ee8e09e55ca8f7b26bf931b8d990e
Dave <dave@webdave.de>
2024-04-25 14:02:56 +0200

    flattening and mapping

--- apps/client/src/app/todos/todo.service.ts
+++ apps/client/src/app/todos/todo.service.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, share, shareReplay, tap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import {
+  concatMap,
+  exhaustMap,
+  map,
+  mergeMap,
+  share,
+  shareReplay,
+  switchMap,
+  tap
+} from 'rxjs/operators';
 import { Toolbelt } from './internals';
 import { Todo, TodoApi } from './models';
 import { TodoSettings } from './todo-settings.service';
@@ -16,9 +25,18 @@ export class TodoService {
     private settings: TodoSettings
   ) {}
 
-  loadFrequently() {
+  loadFrequently(): Observable<Todo[]> {
     // TODO: Introduce error handled, configured, recurring, all-mighty stream
-    return this.query().pipe(
+    // const trigger$ = timer(50, 2000);
+    // const source$ = this.query().pipe(
+    //   tap({ error: () => this.toolbelt.offerHardReload() }),
+    //   shareReplay()
+    // );
+
+    // return trigger$.pipe(exhaustMap(() => source$));
+
+    return timer(50, 1000).pipe(
+      exhaustMap(() => this.query()),
       tap({ error: () => this.toolbelt.offerHardReload() }),
       shareReplay()
     );
