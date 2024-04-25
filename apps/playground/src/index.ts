87a2f55ebce271d70fd15dda44cf9248fb6c2e85
Dave <dave@webdave.de>
2024-04-25 10:09:57 +0200

    ertses observable

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,18 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer, Subscriber } from 'rxjs';
+
+const numbers$ = new Observable(function subscribe(
+  observer: Partial<Observer<number>>
+) {
+  observer.next(1);
+  observer.next(2);
+  // observer.complete();
+  observer.error('Ouch');
+  observer.next(3);
+  observer.next(4);
+});
+
+numbers$.subscribe({
+  next: (v) => console.log(v),
+  error: (err) => console.error(err),
+  complete: () => console.info('Done')
+});
