707cbfb73ecf2534424ba93559b085ebf9ca99f6
Dave <dave@webdave.de>
2024-05-23 10:07:26 +0200

    first Observable

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,23 @@
 import { Observable } from 'rxjs';
+
+// Create observable
+const helloWorld$ = new Observable(function subscribe(observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.error('Ouch!');
+  observer.next(3);
+  observer.next(4);
+});
+
+// Subscribe to an observable
+helloWorld$.subscribe({
+  next(x) {
+    console.log(x);
+  },
+  error(err) {
+    console.error(err);
+  },
+  complete() {
+    console.log('done');
+  }
+});
