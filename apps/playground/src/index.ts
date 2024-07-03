16c3fbf53fd9251485ed3d8e37fb25135c3ab5e7
Dave <dave@webdave.de>
2024-07-03 10:04:53 +0200

    push numbers

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,19 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
+
+const observable = {
+  observer: null
+};
+
+const cnt = new Observable(function (observer: Subscriber<number>) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.error('Ouch!');
+  observer.next(4);
+});
+
+cnt.subscribe({
+  error: (e) => console.error(e),
+  complete: () => console.log('Done!'),
+  next: (data) => console.log(data)
+});
