256706962d6fa8b9a999ef7a1553fd2f3a8bbb7a
Dave <dave@webdave.de>
2024-09-02 10:53:11 +0200

    1st observable

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,15 @@
 import { Observable } from 'rxjs';
+
+const numbers$ = new Observable((observer) => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.complete();
+  observer.next(4);
+});
+
+numbers$.subscribe({
+  next: (data) => console.log('N', data),
+  error: (data) => console.log('E', data),
+  complete: () => console.log('C')
+});
