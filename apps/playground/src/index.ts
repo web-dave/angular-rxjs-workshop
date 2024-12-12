958229c0e82aa9f6c61066d7e28186156938b433
Dave <dave@webdave.de>
2024-12-12 10:03:00 +0100

    myObservable

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,27 @@
 import { Observable } from 'rxjs';
+
+const myObservable = {
+  observer: null,
+  subscribe: function (obs) {
+    myObservable.observer = obs;
+    setTimeout(() => myObservable.next('Hallo'), 3000);
+  },
+  next: function (value: any) {
+    myObservable.observer.next(value);
+  },
+  complete: function () {
+    myObservable.observer.complete();
+    myObservable.observer = null;
+    myObservable.next(44);
+  },
+  error: function (err) {
+    myObservable.observer.error(err);
+    myObservable.observer = null;
+  }
+};
+
+const foo = myObservable.subscribe({
+  next: (data) => console.log(data),
+  complete: () => console.info('Done'),
+  error: (err) => console.error(err)
+});
