002d81f446b36ff55d42398cebc8429a470e55da
Dave <dave@webdave.de>
2024-12-12 14:25:00 +0100

    loadFrequently

--- apps/client/src/app/todos/todo.service.ts
+++ apps/client/src/app/todos/todo.service.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, share, shareReplay, tap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import {
+  concatMap,
+  exhaustMap,
+  map,
+  mergeMap,
+  share,
+  shareReplay,
+  switchMap,
+  tap
+} from 'rxjs/operators';
 import { Toolbelt } from './internals';
 import { Todo, TodoApi } from './models';
 import { TodoSettings } from './todo-settings.service';
@@ -18,7 +27,13 @@ export class TodoService {
 
   loadFrequently() {
     // TODO: Introduce error handled, configured, recurring, all-mighty stream
-    return this.query().pipe(
+
+    // trigger$.pipe(
+    //   mergeMap(()=>source$)
+    // )
+
+    return timer(10, 500).pipe(
+      exhaustMap(() => this.query()),
       share(),
       tap({ error: () => this.toolbelt.offerHardReload() })
     );
