09787ece806ef51a88b1bd41b63b53a92940aec7
Dave <dave@webdave.de>
2024-12-12 11:09:10 +0100

    unsubscribe

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,15 +1,27 @@
 import { Observable } from 'rxjs';
 
 const number$ = new Observable(function (observer) {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.next(4);
-  observer.error('Ouch');
+  let i = 0;
+  const int = setInterval(() => {
+    observer.next(i);
+    console.log('intern', i);
+    i++;
+  }, 1000);
+
+  return function () {
+    clearInterval(int);
+  };
+  // observer.next(1);
+  // observer.next(2);
+  // observer.next(3);
+  // observer.next(4);
+  // observer.error('Ouch');
 });
 
-number$.subscribe({
+const sub = number$.subscribe({
   next: (data) => console.log(data),
   error: (err) => console.error(err),
   complete: () => console.info('Done')
 });
+
+setTimeout(() => sub.unsubscribe(), 3100);
