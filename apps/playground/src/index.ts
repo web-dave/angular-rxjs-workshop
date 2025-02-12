cfb9e37354e8004a9a965740692fbc5b489a7ac9
Dave <dave@webdave.de>
2025-02-12 10:31:00 +0100

    obs und number$

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,1 +1,34 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+console.log('Moin');
+
+const obs = {
+    value: null,
+    observer: null,
+    subscribe: function (observer) {
+        obs.observer = observer;
+    },
+    next: (value) => {
+        obs.observer.next(value);
+    },
+    complete: () => {
+        obs.observer.complete();
+        obs.observer = null;
+    },
+};
+
+const number$ = new Observable(
+    (observer: Observer<number>) => {
+        observer.next(1)
+        observer.next(2)
+        observer.error('Ouch')
+        observer.next(3)
+        observer.next(4)
+    }
+)
+
+number$.subscribe({
+    next: data => console.log(data),
+    error: data => console.log(data),
+    complete: () => console.log('Done'),
+})
